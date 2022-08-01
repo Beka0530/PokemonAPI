c5a65ec3646ec2fd38b555d42bd0227394739cbe
Beka0530 <0555991093b@gmail.com>
2022-08-01 11:49:54 +0600

    pokemon-api

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import '../app/App.css';
+import Main from '../Main';
+import '../style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
